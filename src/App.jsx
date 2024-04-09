1a4384ceb2737b5d9cf9decf5e86d1f81be325b9
jossjic <jossjic_03@hotmail.com>
2024-04-09 10:40:14 -0600

    cadCheckCounter implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import { CadCheckCounter } from "./components/cadCheckCounter";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <>a</>;
+  return (
+    <>
+      <CadCheckCounter unit="test" amount="1" />
+    </>
+  );
 }
 
 export default App;
