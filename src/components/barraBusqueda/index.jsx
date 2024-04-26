7b3ad93c6db45aad1ea4729f9c190e2a30274af2
JacquiVA02 <a01736339@tec.mx>
2024-04-25 21:26:14 -0600

    Componente seleccionar Fechas

--- src/components/barraBusqueda/index.jsx
+++ src/components/barraBusqueda/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./barrabusqueda.css";
-import searchIcon from './search-icon.png';
+/*import searchIcon from './search-icon.png';*/
 
 export function barraBusqueda() {
     return (
--- src/components/selectDate/index.jsx
+++ src/components/selectDate/index.jsx
@@ -1,9 +1,49 @@
 import "./selectDate.css";
+import { CadCheckCounter } from "../../components/cadCheckCounter";
+import { GeneralButton } from "../../components/button";
+import { barraBusqueda } from "../../components/barraBusqueda";
 
 export function SelectDate({ unit, amount }) {
+  const dates = [
+    {id: 1, caducidad:"01/05/2002", cantidad:"NA"},
+    {id: 2, caducidad:"08/07/2024", cantidad: 500}
+];
+
   return (
     <div className="checkCard_selectDate">
-      
+      <table className="generalTable">
+        <td>
+          <tr>
+            <CadCheckCounter unit="test" amount="1"></CadCheckCounter>
+          </tr>
+          <tr>
+            <GeneralButton textElement="Confirmar" path="" color="#4FA725"></GeneralButton>
+          </tr>
+          <tr>
+            <GeneralButton textElement="Cancelar" path="" color="#E14040"></GeneralButton>
+          </tr>
+          <tr></tr>
+          <tr>
+            <GeneralButton textElement="Agregar Caducidad" path="" color="#5982C0"></GeneralButton> 
+          </tr>
+        </td>
+        <td>
+          <table className="productsTable">
+                        <thead>
+                            <th>Caducidad</th>
+                            <th>Cantidad Seleccionada</th>
+                        </thead>
+                        <tbody>
+                        {dates.map(date => (
+                            <tr key={date.id}>
+                            <td>{date.caducidad}</td>
+                            <td>{date.cantidad}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+        </td>
+      </table>  
     </div>
   );
 }
