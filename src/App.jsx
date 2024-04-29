40e35718edbc3c2c5e97702288c222d64ecfcdad
LACG24 <lacg24@outlook.com>
2024-04-29 10:36:20 -0600

    merge fix

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,9 @@ import { TestScreen } from "./screens/testScreen";
 import { MainPage } from "./screens/mainPage";
 import { UserPage } from "./screens/adminUserPage";
 import { AdminPage } from "./screens/adminPage";
-<<<<<<< HEAD
 import { CheckDateAdd } from "./screens/checkDateAdd";
 import { CheckDateDelete } from "./screens/checkDateDelete";
-=======
 import { RestorePass } from "./screens/restorePass";
->>>>>>> 682b7bce5f0ecd7d7f82d9dbe04bc6b96bc73d41
 import { CreateUser } from "./screens/createUser";
 import { AddProduct } from "./screens/addProduct";
 
@@ -30,16 +27,12 @@ function App() {
           <Route path="/mainPage" element={<MainPage />} />
           <Route path="/adminUserPage" element={<UserPage />} />
           <Route path="/adminPage" element={<AdminPage />} />
-<<<<<<< HEAD
           <Route path="/checkDateAdd" element={<CheckDateAdd/>}/>
           <Route path="/checkDateDelete" element={<CheckDateDelete/>}/>
           <Route path="/createUser" element={<CreateUser/>}/>
-=======
           <Route path="/restorePass" element={<RestorePass />} />
->>>>>>> 682b7bce5f0ecd7d7f82d9dbe04bc6b96bc73d41
           <Route path="/createUser" element={<CreateUser />} />
           <Route path="/addProduct" element={<AddProduct />} />
-
         </Routes>
       </Router>
     </div>
