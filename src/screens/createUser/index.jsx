06ef75777919a3dab2f96d9c6300eaf985414405
Augusto Gomez <augustogomezmaxil2003@gmail.com>
2024-05-02 10:28:48 -0600

    Registro de usuarios (Backend listo)

--- src/screens/createUser/index.jsx
+++ src/screens/createUser/index.jsx
@@ -1,15 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import "./createUser.css";
 import { CreateInput } from "../../components/createInput";
 import { Guide } from "../../components/guide";
 import { GeneralButton } from "../../components/button";
 
 export const CreateUser = () => {
+
+  const [formData, setFormData] = useState({
+    u_id: "",
+    u_nombre : "",
+    u_apellidos: "",
+    u_email: "",
+    u_contraseña: "",
+    u_rol: 0,
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async () => {
+    try {
+      console.log(formData);
+      const response = await fetch("http://3.20.237.82:3000/usuarios", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      if (!response.ok) {
+        throw new Error("Error al agregar el usuario");
+      }
+      // Manejar el éxito de la inserción
+      console.log("Usuario agregado correctamente");
+    } catch (error) {
+      console.error("Error al agregar el usuario:", error);
+    }
+  };
+
+
   return (
     <div className="createUser">
-      <Guide message="No olvides llenar todos los campos para el registro" />
-      <CreateInput />
-      <GeneralButton textElement="Crear usuario" />
+      <div className="mensaje">
+        <Guide message="No olvides llenar todos los campos para el registro" />
+      </div>
+      <div className='createUser-container'>
+      <br/>
+        <div className="createInput">
+              <p>Nombre de usuario</p>
+              <input name="u_id" value={formData.u_id} type="text" onChange={handleChange}/>
+              <p>Nombre</p>
+              <input name="u_nombre" value={formData.u_nombre} type="text" onChange={handleChange}/>
+              <p>Apellidos</p>
+              <input name="u_apellidos" value={formData.u_apellidos} type="text" onChange={handleChange}/>
+              <p>Correo</p>
+              <input name="u_email" value={formData.u_email} type="email" onChange={handleChange}/>
+              <p>Contraseña</p>
+              <input name="u_contraseña" value={formData.u_contraseña} type="password" onChange={handleChange}/>
+        </div>
+        <br/>
+        <GeneralButton textElement="Crear usuario" onClick={handleSubmit}/>
+      </div>
     </div>
   );
 };
