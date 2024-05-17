448d2baf3ee75e5e63fba3232d4c15e8ed959d6b
ChrisJimenez29 <A01736302@tec.mx>
2024-05-17 12:15:03 -0600

    Correcion

--- src/components/guide2/index.jsx
+++ src/components/guide2/index.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import "./guide2.css";
 import humanoIcon from "../../assets/img/humanIcon.svg";
 
-export function Guide({ message }) {
+export function Guide2({ message }) {
   return (
     <div className="info-banner">
       <img src={humanoIcon} alt="Icono de humano" />
