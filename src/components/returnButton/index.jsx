024b23b5b414e7782c4ee643d12a69533ab6f9da
LACG24 <lacg24@outlook.com>
2024-05-23 08:40:55 -0600

    arreglado return button en checkdatedelete

--- src/components/returnButton/index.jsx
+++ src/components/returnButton/index.jsx
@@ -3,15 +3,22 @@ import "./returnButton.css";
 import returnImage from "../../assets/img/returnImage.png";
 import { useNavigate } from "react-router-dom";
 
-export function ReturnButton({ textElement }) {
+export function ReturnButton({ textElement, onClick }) {
   const [color, setColor] = useState("#D9D9D9");
   let navigate = useNavigate();
+
+  const handleClick = () => {
+    if (onClick) {
+      onClick();
+    }
+    navigate(-1); 
+  };
 
   return (
     <div className="returnButtonContainer">
       <button
         className="returnButton"
-        onClick={() => navigate(-1)}
+        onClick={handleClick}
         style={{
           backgroundColor: color,
           color: "white",
