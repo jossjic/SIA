11ef909c8fbd9c599ff87cf3d9c96b9c95f8a598
LACG24 <lacg24@outlook.com>
2024-06-06 22:34:39 -0600

    yves

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,27 +1,49 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { logout } from "../generalFunctions";
 
 export const ProtectedRoute = ({ children }) => {
   const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
 
   useEffect(() => {
-    const checkSession = () => {
+    const checkSession = async () => {
       const isAuthenticated = document.cookie.includes("userCookieSIA");
 
       setIsLoading(false);
 
       if (!isAuthenticated) {
-        // Aquí puedes manejar la lógica para cuando el usuario no está autenticado
         navigate("/login");
       } else {
         const userCookieValue = getCookieValue("userCookieSIA");
         localStorage.setItem("userId", userCookieValue);
+
+        try {
+          // Verifica si el user_id existe en la base de datos usando fetch
+          const response = await fetch(`http://${API_HOST}:${API_PORT}/validate`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ id: userCookieValue }),
+          });
+
+          if (response.status === 200) {
+            console.log("User validated");
+          } else if (response.status === 401) {
+            console.log("User not found");
+            logout(navigate);
+          } else {
+            console.error("Server error");
+          }
+        } catch (error) {
+          console.error("An error occurred during validation:", error);
+        }
       }
     };
 
     checkSession();
-  }, []);
+  }, [navigate]);
 
   if (isLoading) {
     // Indicador de carga mientras se verifica la sesión
